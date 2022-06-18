c9d0239bd313aa8b28bab38fba08ea3b224c6a44
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2022-06-17 23:27:45 -0400

    Write SQL code with cockroach
    Co-authored-by: Chantal <chalory@users.noreply.github.com>

--- db.py
+++ db.py
@@ -1,6 +1,3 @@
-"""
-Test psycopg with CockroachDB.
-"""
 from collections import defaultdict
 
 import json
@@ -14,18 +11,68 @@ import logging
 import psycopg2
 from psycopg2.errors import SerializationFailure
 
-conn = psycopg2.connect("postgresql://chantal:wafflehacks2022@free-tier6.gcp-asia-southeast1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dwafflehacks-2805")
+conn = psycopg2.connect(
+    "postgresql://chantal:wafflehacks2022@free-tier6.gcp-asia-southeast1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dwafflehacks-2805")
 
-def info(conn):
+
+def initDB(conn):
     with conn.cursor() as cur:
         cur.execute(
-             "CREATE TABLE IF NOT EXISTS user_info (email VARCHAR(319), password VARCHAR(50), username VARCHAR(50), counter INT DEFAULT 0)")
-      
-        logging.debug("create_accounts(): status message: %s",
+            "CREATE TABLE IF NOT EXISTS user_info (\
+                user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),\
+                email VARCHAR(319), password VARCHAR(50))"),
+
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS reviews (\
+                rev_id UUID PRIMARY KEY DEFAULT gen_random_uuid(), \
+                revText VARCHAR(1000), restaurant VARCHAR(100), \
+                dietary_rest VARCHAR(1000), rating FLOAT)"),
+
+        logging.debug("initDB(): status message: %s",
                       cur.statusmessage)
-       
-    
-    conn.commit()  
+
+    conn.commit()
 
 
-info(conn)
+def addUser(conn, email, password):
+    with conn.cursor() as cur:
+
+        cur.execute(
+            f"INSERT INTO user_info (email, password) VALUES ('{email}', '{password}')")
+
+        logging.debug("addUser(): status message: %s")
+
+    conn.commit()
+
+
+def createReview(revText, restaurant, dietary_rest, rating):
+    with conn.cursor() as cur:
+
+        cur.execute(
+            f"INSERT INTO reviews (revText, restaurant, dietary_rest, rating) \
+            VALUES ('{revText}', '{restaurant}', '{dietary_rest}', '{rating}')")
+
+        logging.debug("createReview(): status message: %s")
+
+    conn.commit()
+
+
+def getRating(restaurant, dietary_rest):
+    with conn.cursor() as cur:
+
+        cur.execute(
+            f"SELECT AVG(rating) FROM reviews \
+            WHERE restaurant='{restaurant}' AND dietary_rest='{dietary_rest}'")
+        result = cur.fetchone()
+        logging.debug("createReview(): status message: %s")
+
+    conn.commit()
+
+    return result[0]
+
+
+initDB(conn)
+addUser(conn, email='hi@gmail.com', password='pass')
+createReview(revText='This place was good!',
+             restaurant='subway', dietary_rest='vegan', rating=5)
+print(getRating('subway', 'vegan'))
