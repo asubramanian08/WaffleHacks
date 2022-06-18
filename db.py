473e440f29a816d4ca78c5bccad5fd2ab797051e
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2022-06-18 19:26:37 -0400

    sync the outer db.py

--- db.py
+++ db.py
@@ -40,7 +40,7 @@ def addUser(conn, email, password):
         cur.execute(
             f"INSERT INTO user_info (email, password) VALUES ('{email}', '{password}')")
 
-        logging.debug("addUser(): status message: %s")
+        logging.debug("addUser(): status message: %s", cur.statusmessage)
 
     conn.commit()
 
@@ -52,7 +52,7 @@ def createReview(revText, restaurant, dietary_rest, rating):
             f"INSERT INTO reviews (revText, restaurant, dietary_rest, rating) \
             VALUES ('{revText}', '{restaurant}', '{dietary_rest}', '{rating}')")
 
-        logging.debug("createReview(): status message: %s")
+        logging.debug("createReview(): status message: %s", cur.statusmessage)
 
     conn.commit()
 
@@ -64,15 +64,56 @@ def getRating(restaurant, dietary_rest):
             f"SELECT AVG(rating) FROM reviews \
             WHERE restaurant='{restaurant}' AND dietary_rest='{dietary_rest}'")
         result = cur.fetchone()
-        logging.debug("createReview(): status message: %s")
+        logging.debug("getRating(): status message: %s", cur.statusmessage)
 
     conn.commit()
 
     return result[0]
 
 
-initDB(conn)
-addUser(conn, email='hi@gmail.com', password='pass')
-createReview(revText='This place was good!',
-             restaurant='subway', dietary_rest='vegan', rating=5)
-print(getRating('subway', 'vegan'))
+def getReviews(restaurant, dietary_rest):
+    with conn.cursor() as cur:
+
+        cur.execute(
+            f"SELECT revText FROM reviews \
+            WHERE restaurant='{restaurant}' AND dietary_rest='{dietary_rest}'")
+        result = [i[0] for i in cur.fetchall()]
+        logging.debug("getReviews(): status message: %s", cur.statusmessage)
+
+    conn.commit()
+
+    return result
+
+
+def validate_login(conn, email, password):
+
+    with conn.cursor() as cur:
+
+        cur.execute(
+            f"SELECT user_id FROM user_info WHERE email='{email}' AND password='{password}'"
+        )
+        result = cur.fetchone()
+        conn.commit()
+        print(result)
+
+
+def resetTables(conn):
+    # """ Debugging purposes """
+    with conn.cursor() as cur:
+        cur.execute("DROP TABLE user_info"),
+
+        cur.execute("DROP TABLE reviews"),
+
+        logging.debug("resetTables(): status message: %s", cur.statusmessage)
+
+    conn.commit()
+
+
+# initDB(conn)
+# addUser(conn, email='hi@gmail.com', password='pass')
+# # resetTables(conn)
+# createReview(revText='This place was good!',
+#              restaurant='subway', dietary_rest='vegan', rating=5)
+# print(getRating('subway', 'vegan'))
+# print(getReviews('subway', 'vegan'))
+# validate_login(conn, email='hi@gmail.com', password='pass')
