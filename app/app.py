bda006baa480b19102dc44268c3d41cfb9e72c2e
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2022-06-18 21:49:21 -0400

    reformat

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ def register():
     t_email = request.form.get("t_Email", "")
     t_password = request.form.get("t_Password", "")
 
-    # # check for blanks
+    # check for blanks
     if t_email == "":
         t_message = "Please fill in your email address"
         return render_template("register.html")
@@ -44,6 +44,7 @@ def signin():
         return redirect(url_for("/"))
     return render_template("login.html")
 
+
 @app.route("/restriction", methods=["POST", "GET"])
 def restriction():
     restriction = form.get('restriction')
@@ -52,25 +53,17 @@ def restriction():
     return render_template("restriction.html")
 
 
-
-
 @app.route("/search_map", methods=["POST", "GET"])
 def placeholder():
     location = form.get(whatever user puts in form)
-    #use geolocation API to convert location to proper format
+    # use geolocation API to convert location to proper format
     query_info = maps.get_restaurants(location)
     names_list = query_info[for name in name return query_info[0]]
     addresses_list =
-    ratings = 
+    ratings =
     reviews = db.getReviews(restaurant, dietary_rest)
 
     return render_template("maps.html", query_info)
-
-
-    
-
-
-
 
 
 app.run(host='0.0.0.0', port=81)
--- app/map.py
+++ app/map.py
@@ -6,7 +6,7 @@ def get_restaurants(location):
     # these are the parameters we have to set for search query to google places api
     # latitude and longitude of user inputted location, set manually to UCSD, later use
     # geolocation api to change:
-    #loc = "32.8801,-117.2340"
+    # loc = "32.8801,-117.2340"
     # radius set as default to 50000 meters, can change later to user input
     radius = "50000"
     searchType = "restaurant"
@@ -26,19 +26,21 @@ def get_restaurants(location):
     places.extend(response_json["results"])
     return places
 
-#get a list of the names of the restaurants
+
 def get_rest_names(restaurants):
+    """ Get a list of the names of the restaurants. """
     rest_names = []
     for rest in restaurants:
-        try: 
+        try:
             rest_names.append(restaurants['name'])
         except:
             rest_names.append('none')
-    
+
     return rest_names
 
-#get a list of the addresses of the restaurants
+
 def get_rest_addresses(restaurants):
+    """ Get a list of the addresses of the restaurants. """
     rest_addresses = []
     for rest in restaurants:
         try:
@@ -47,8 +49,9 @@ def get_rest_addresses(restaurants):
             rest_addresses.append('none')
     return rest_addresses
 
-#get the restaurant hours
+
 def get_rest_hours(restaurants):
+    """ Get the restaurant hours. """
     rest_hours = []
     for rest in restaurants:
         try:
@@ -58,6 +61,4 @@ def get_rest_hours(restaurants):
     return rest_hours
 
 
-
-
-print(get_rest_names(get_restaurants(("32.8801,-117.2340")))
+print(get_rest_names(get_restaurants(("32.8801,-117.2340"))))
